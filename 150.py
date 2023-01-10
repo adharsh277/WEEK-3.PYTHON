3e3b1e26003bdad1c32d8f23f51331ffc38356c3
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2023-01-10 19:48:37 +0530

    Create 150.py

---
+++ 150.py
@@ -0,0 +1,18 @@
+# function 
+# list , reverse_str = True
+# list
+
+def func(l, **kwargs):
+    if kwargs.get('reverse_str') == True:
+        return [name[::-1].title() for name in l]
+    else:
+        return [name.title() for name in l]
+
+
+names = ['harshit', 'mohit']
+print(func(names, reverse_str = True))
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
